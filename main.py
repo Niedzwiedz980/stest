7e7fd9a0f33491696607a137024207497ed6241f
Niedzwiedz980 <kuba123le@gmail.com>
2022-02-26 13:32:55 +0100

    dodanie zmienych i funkcji

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import sys
+import math
 
 print(sys.version)
 a=15
+b='wizualizacja danych'
+print(b)
+print(type(b))
+c=15.5
+print(c)
+print(type(c))
+d=3+2j
+print(d)
+print(type(d))
+
+e=7.5
+f=3
+print(e//f) #dzielenie całkowite
+print(e**f) #potęgowanie
+
+print(math.pow(e, f))
